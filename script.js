6cd3a5434473431e04390abde18ec4c6e0c9053a
Muhammadumar <khilolamoon@gmail.com>
2024-08-01 12:07:22 +0500

    added mobile menu

--- script.js
+++ script.js
@@ -32,3 +32,13 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+const m_menu = document.getElementById("m-menu");
+const drop_down = document.getElementById("drop-down");
+m_menu.addEventListener("click", () => {
+  if (drop_down.style.display === "none") {
+    drop_down.style.display = "block";
+  } else {
+    drop_down.style.display = "none";
+  }
+});
